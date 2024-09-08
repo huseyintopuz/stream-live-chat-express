95c5150aeffa0c7c250106f6d50a3faae0b0bdcd
boceksoft_ofis <huseyin.topuz35@gmail.com>
2024-09-08 11:36:57 +0300

    firebase and firestore integrated

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import { genSaltSync, hashSync } from "bcryptjs";
+import { compareSync, genSaltSync, hashSync } from "bcryptjs";
 import { StreamChat } from "stream-chat";
+import { db } from "../firebase";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 
 dotenv.config();
 
@@ -13,13 +15,21 @@ app.use(express.json());
 
 const salt = genSaltSync(10);
 
-interface User {
-    id: string;
-    email: string;
-    hashed_password: string;
+const usersCollectionRef = collection(db, "users");
+// Async function to fetch data from Firestore
+async function getUserByEmail(email: string) {
+    const q = query(usersCollectionRef, where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0].data();
+        return {
+            id: userDoc.id,
+            email: userDoc.email,
+            hashed_password: userDoc.hashed_password,
+        };
+    }
+    return null;
 }
-
-const USERS: User[] = [];
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
@@ -30,47 +40,53 @@ app.post("/register", async (req, res) => {
         return res.status(400).json({ message: "Missing username or email" });
     }
 
-    if (USERS.find(user => user.email === email)) {
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
         return res.status(400).json({ error: "User already exists" });
     }
 
     if (password.length < 8) {
         return res.status(400).json({ error: "Password must be at least 8 characters long" });
     }
-    try {
-        const hashed_password = hashSync(password, salt);
-        const id = Math.random().toString(36).slice(2);
-        console.log(id);
-        const newUser = {
-            id, email, hashed_password
-        }
-        USERS.push(newUser);
 
-        await serverClient.upsertUsers([{
-            id,
-            email,
-            name: email,
-        }]);
-
-        const token = serverClient.createToken(id);
-        return res.status(200).json({
-            token,
-            user: { id, email }
-        });
-
-    } catch (err) {
-        res.status(500).send({ error: "User already exists" });
+    const hashed_password = hashSync(password, salt);
+    const id = Math.random().toString(36).slice(2);
+    const newUser = {
+        id, email, hashed_password
     }
+
+    await addDoc(usersCollectionRef, {
+        newUser
+    });
+
+    await serverClient.upsertUsers([{
+        id,
+        email,
+        name: email,
+    }]);
+
+    const token = serverClient.createToken(id);
+    return res.status(200).json({
+        token,
+        user: { id, email }
+    });
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
     const { email, password } = req.body;
-    const user = USERS.find(user => user.email === email);
-    const hashed_password = hashSync(password, salt);
-    if (!user || user.hashed_password !== hashed_password) {
-        return res.status(400).json({ error: "Invalid username or password" });
+    const user = await getUserByEmail(email);
+    console.log(user);
+
+    if (!user) {
+        return res.status(400).json({ error: "Invalid email" });
     }
+    if (user && !compareSync(password, user.hashed_password)) {
+        return res.status(400).json({ error: "Invalid password" });
+    }
+
     const token = serverClient.createToken(user.id);
+
     return res.status(200).json({
         token,
         user: { id: user.id, email: user.email }
